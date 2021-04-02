2043cdce2a491dcbb4239cda56f5a6bca7119b9c
Bella <bella.wei@sjsu.edu>
2021-04-02 09:46:40 -0700

    617,938

---
+++ 938.RangeSumofBST.java
@@ -0,0 +1,49 @@
+//  938. Range Sum of BST
+
+
+class Solution {
+    int ans;
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        ans = 0;
+        dfs(root, L, R);
+        return ans;
+    }
+
+    public void dfs(TreeNode node, int L, int R) {
+        if (node != null) {
+            if (L <= node.val && node.val <= R)
+                ans += node.val;
+            if (L < node.val)
+                dfs(node.left, L, R);
+            if (node.val < R)
+                dfs(node.right, L, R);
+        }
+    }
+}
+
+
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        int ans = 0;
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if (node != null) {
+                if (L <= node.val && node.val <= R)
+                    ans += node.val;
+                if (L < node.val)
+                    stack.push(node.left);
+                if (node.val < R)
+                    stack.push(node.right);
+            }
+        }
+        return ans;
+    }
+}
+
+// Time Complexity: O(N), where NN is the number of nodes in the tree.
+
+// Space Complexity: O(N)
+
+
